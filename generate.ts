7c0ed5a876d258fa4c5de785d5f36a6496e4fbc4
Your Name <you@example.com>
2024-05-13 09:24:25 +0900

    base64形式に変更

--- generate.ts
+++ generate.ts
@@ -7,17 +7,20 @@ const cloudfrontDistributionDomain = "https://d1ufgkk53fjk8u.cloudfront.net";
 const s3ObjectKey = "*";
 const url = `${cloudfrontDistributionDomain}/${s3ObjectKey}`;
 // Private Keyをopensslで生成したものを使用するようにした
-const privateKey = fs.readFileSync("./private_key.pem");
+const privateKey = fs.readFileSync("./private_key_base64", {
+  encoding: "utf-8",
+});
 const keyPairId = "K1QOTCNUEB4GMO";
 const dateLessThan = "2025-01-01";
 const dateGreaterThan = "2000-01-01";
+const decode = Buffer.from(privateKey, "base64").toString("utf-8");
 
 const cookies = getSignedCookies({
   url,
   keyPairId,
   dateLessThan,
   dateGreaterThan,
-  privateKey,
+  privateKey: decode,
 });
 
 const command = `curl -H 'Cookie: CloudFront-Policy=${cookies["CloudFront-Policy"]};CloudFront-Signature=${cookies["CloudFront-Signature"]};CloudFront-Key-Pair-Id=${keyPairId}' ${cloudfrontDistributionDomain}/dog.jpg --output dog.jpg`;
